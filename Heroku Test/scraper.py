d15782421940f44c171e4bfe032d97a02c5b3330
georgeboghez <35366691+georgeboghez@users.noreply.github.com>
2020-03-28 14:14:16 +0200

    renamed scaper.py and removed useless code from it

--- Heroku Test/scraper.py
+++ Heroku Test/scraper.py
@@ -1,18 +1,6 @@
 import os
-import pymongo
-import pprint
-import emoji
 import json
 import requests
-
-class App(dict):
-    def __str__(self):
-        return json.dumps(self)
-
-
-def deEmojify(inputString):
-    return inputString.encode('ascii', 'ignore').decode('ascii')
-
 
 if __name__ == '__main__':
     os.environ['NO_PROXY'] = '127.0.0.1'
