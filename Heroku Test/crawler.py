a5b01590db60b4cf1992ef80720fffb99cf7f9b1
georgeboghez <35366691+georgeboghez@users.noreply.github.com>
2020-03-28 14:35:11 +0200

    Fixed tweets' text field being truncated; changed the request url from the scraper example

--- Heroku Test/crawler.py
+++ Heroku Test/crawler.py
@@ -2,9 +2,9 @@ import tweepy as tw
 from random import shuffle
 import json
 
-consumer_key        = 'CmQS1BFO5D38aYu38isgYfQSM'
-consumer_secret     = '0BT4Pvkox9y4USuKmzsh6IzzfymdMcPrCKANnYVc82qXZMRn7D'
-access_token        = '1242346386756112384-rBnQ65dlWwgbVQp6Me6op8AEqogEgn'
+consumer_key = 'CmQS1BFO5D38aYu38isgYfQSM'
+consumer_secret = '0BT4Pvkox9y4USuKmzsh6IzzfymdMcPrCKANnYVc82qXZMRn7D'
+access_token = '1242346386756112384-rBnQ65dlWwgbVQp6Me6op8AEqogEgn'
 access_token_secret = 'P6kfsxgeTkL7e6vifWf5VJmDQVKIKMehPyJLPJ16RcglK'
 
 
@@ -15,38 +15,42 @@ api = tw.API(auth, wait_on_rate_limit=True)
 tweetList = list()
 
 
-def getTweets (search_word, date_since, count):
+def getTweets(search_word, date_since, count):
     tweets = tw.Cursor(api.search,
                        q=search_word,
                        lang="en",
+                       tweet_mode='extended',
                        since=date_since).items(count)
     return tweets
 
 
-def getTweetsByUsers (user_ids, date_since, count, query=""):
+def getTweetsByUsers(user_ids, date_since, count, query=""):
     tweets = list()
     for user in user_ids:
         tweets += tw.Cursor(api.user_timeline,
-                            q = query,
-                           id = user,
-                           since=date_since).items(count)
+                            q=query,
+                            id=user,
+                            tweet_mode='extended',
+                            since=date_since).items(count)
     return tweets
 
 
 def displayTweets(tweets):
     for t in tweets:
-        print(t.text + "\n")
+        print(t.full_text + "\n")
 
 
 def addTweetLists(*list):
     global tweetList
     for l in list:
-        tweetList += [json.dumps(tweet._json, indent=4, sort_keys=True) for tweet in l]
+        tweetList += [json.dumps(tweet._json, indent=4,
+                                 sort_keys=True) for tweet in l]
     shuffle(tweetList)
 
 
 def main():
-    user_list = ["EU_Comission", "BernieSanders", "JoeBiden", "elonmusk", "tconnellyRTE", "BarackObama", "Samsung", "NASA"]
+    user_list = ["EU_Comission", "BernieSanders", "JoeBiden",
+                 "elonmusk", "tconnellyRTE", "BarackObama", "Samsung", "NASA"]
 
     user_news = getTweetsByUsers(user_list, "2020-03-20", 4)
     news_tweets = getTweets("coronavirus", "2020-03-20", 10)
@@ -57,7 +61,9 @@ def main():
     europe = getTweets("#europe", "2020-03-10", 4)
     br_news_tweets = getTweets("#breaking-news", "2020-03-20", 30)
 
-    addTweetLists(br_news_tweets, user_news, news_tweets, ad_tweets, economy, olympics, russia, europe)
+    displayTweets(user_news)
+    addTweetLists(br_news_tweets, user_news, news_tweets,
+                  ad_tweets, economy, olympics, russia, europe)
 
     return tweetList
 
--- Heroku Test/scraper.py
+++ Heroku Test/scraper.py
@@ -3,6 +3,6 @@ import json
 import requests
 
 if __name__ == '__main__':
-    os.environ['NO_PROXY'] = '127.0.0.1'
-    response = requests.get('http://127.0.0.1:5000/all/2')
-    print(json.loads(response.json())[0]['_id'])
+    # os.environ['NO_PROXY'] = '127.0.0.1'
+    response = requests.get('http://ip2020.herokuapp.com/all/2')
+    print(json.loads(response.json())[0]['full_text'])
