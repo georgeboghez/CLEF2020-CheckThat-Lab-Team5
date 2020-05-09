d2e16e09c1105ebeffe541500e6b5b8050ccceb0
georgeboghez <35366691+georgeboghez@users.noreply.github.com>
2020-05-09 11:40:55 +0300

    deleted useless files

--- Heroku Test/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\proiect python\CLEF2020-CheckThat-Lab-Team5-master\Heroku Test\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
